80658ee6687884b976eab820c40b60873844cc22
ToF- <cthibauttof@gmail.com>
2026-03-20 11:04:39 +0100

    redo : matches

--- src/mastermind.fs
+++ src/mastermind.fs
@@ -10,9 +10,12 @@
 max-pegs (first-codeword) value first-codeword
 max-pegs (last-codeword) value last-codeword
 
+: unit/tenth ( cw -- c,cw' )
+    10 /mod ;
+
 : (next-codeword) ( cw,c -- cw' )
     if
-        10 /mod
+        unit/tenth
         swap dup max-colors < if
             1+
         else
@@ -28,3 +31,47 @@ max-pegs (last-codeword) value last-codeword
         drop 0
     then ;
 
+create factors
+    1 , 10 , 100 , 1000 , 10000 , 100000 , 1000000 , 10000000 , 100000000 ,
+
+: factor ( n -- 10^n )
+    1- cells factors + @ ;
+
+: (color-tally) ( acc,cw -- colors )
+    ?dup if
+        unit/tenth
+        swap factor
+        rot + swap
+        recurse
+    then ;
+
+: color-tally ( cw -- colors )
+    0 swap (color-tally) ;
+
+: hits ( cw,cw' -- n )
+    0 -rot color-tally
+    swap color-tally
+    max-colors 0 do
+        unit/tenth
+        swap rot unit/tenth
+        -rot min
+        -rot 2swap + -rot
+    loop
+    2drop ;
+
+: matches ( cw,cw' -- n )
+    0 -rot
+    max-pegs 0 do
+        unit/tenth
+        swap rot unit/tenth
+        -rot = if
+            rot 1+ -rot
+        then
+    loop
+    2drop ;
+
+: match ( cw,cw' -- r )
+    2dup matches -rot
+    hits over -
+    swap 10 * + ;
+    
--- test/mastermind-test.fs
+++ test/mastermind-test.fs
@@ -8,7 +8,7 @@ page
 max-colors 6 ?s max-pegs 4 ?s first-codeword 1111 ?s
 6 (first-codeword) 111111 ?s
 
-\ last-codeword
+\ last codeword
 
 last-codeword 6666 ?s
 
@@ -21,7 +21,35 @@ last-codeword 6666 ?s
 6665 next-codeword 6666 ?s
 6666 next-codeword ?false
 
+\ color tally
 
+1246 color-tally 101011 ?s
+1122 color-tally 000022 ?s
+6666 color-tally 400000 ?s
+3335 color-tally 010300 ?s
+
+\ hits : number of colors found, regardless of position
+
+1246 1126 hits 3 ?s
+6666 6666 hits 4 ?s
+1234 5555 hits 0 ?s
+4444 1236 hits 0 ?s
+
+\ matches : number of colors found in correct position
+
+1246 1345 matches 2 ?s
+1234 1234 matches 4 ?s
+1111 2345 matches 0 ?s
+1234 4321 matches 0 ?s
+1111 1222 matches 1 ?s
+
+\ match : matches * 10 + misses
+
+1246 1126 match 21 ?s
+1234 5555 match 00 ?s
+1122 1234 match 11 ?s
+1111 3146 match 10 ?s
+1234 4321 match 04 ?s
 
 bye
 
