3c8c516899b83b3718364fda90b933d1967bd1fc
Christophe Thibaut <cthibauttof@gmail.com>
2026-03-20 09:49:15 +0100

    restart without set

---
+++ src/set-mastermind.fs
@@ -0,0 +1,242 @@
+6 constant max-colors
+4 constant max-pegs
+
+
+: c++! ( addr -- )
+    dup c@ 1+ swap c! ;
+
+: ++! ( addr -- )
+    1 swap +! ;
+
+: ^ ( x,y -- x^y )
+    1 swap 0 do over * loop nip ;
+
+max-colors max-pegs ^ constant max-codewords
+
+: number>pegs ( n -- p1,p2,p3,p4 )
+    max-pegs 1- 0 do
+        max-colors /mod
+    loop ;
+
+: pegs>codeword ( p1,p2,p3,p4 -- cw )
+    0
+    max-pegs 0 do
+        10 * swap 1+ +
+    loop ;
+
+: number>codeword ( n -- cw )
+    number>pegs pegs>codeword ;
+
+: codeword>digits ( n -- d1,d2,d3,d4 )
+    max-pegs 1- 0 do
+        10 /mod
+    loop ;
+
+: digits>number ( d1,d2,d3,d4 -- n )
+    0 max-pegs 0 do
+        max-colors * swap 1- +
+    loop ;
+
+: codeword>number ( cw -- n )
+    codeword>digits digits>number ;
+
+: offset,bits ( n -- offest,bits )
+    8 /mod 1 rot lshift ;
+
+: bitset-member? ( n,bitset -- f )
+    swap offset,bits
+    -rot + c@ and ;
+
+: bitset-insert ( n,bitset -- )
+    swap offset,bits
+    -rot + dup c@
+    rot or
+    swap c! ;
+
+: bitset-remove ( n,bitset -- )
+    swap offset,bits 255 xor
+    -rot + dup c@
+    rot and
+    swap c! ;
+
+: codewords ( <name> )
+    create -1 , max-codewords 8 / allot ;
+
+: codewords-start! ( cws -- )
+    -1 swap ! ;
+
+: all-codewords! ( cws -- )
+    dup cell+ max-codewords 8 / 255 fill
+    codewords-start! ;
+
+: empty-codewords! ( cws -- )
+    dup cell+ max-codewords 8 / erase
+    codewords-start! ;
+
+: (codeword-number) ( cw,cws -- n,bitset )
+    cell+ swap codeword>number swap ;
+
+: codeword-member? ( cw,cws -- f )
+    (codeword-number) bitset-member? ;
+
+: codeword-remove ( cw,cws -- )
+    (codeword-number) bitset-remove ;
+
+: codeword-insert ( cw,cws -- )
+    (codeword-number) bitset-insert ;
+
+: (next-codeword) ( cws -- )
+    begin
+        1 over +!
+        dup @ dup max-codewords < -rot
+        over cell+ bitset-member? 0=
+        rot and while 
+    repeat drop ;
+
+: next-codeword ( cws -- cw|f )
+    dup (next-codeword)
+    @ dup max-codewords < if
+        number>codeword
+    else
+        drop 0
+    then ;
+
+: matches ( cw1,cw2 -- n )
+    0 -rot
+    max-pegs 0 do
+        10 /mod rot 10 /mod rot
+        2swap = if
+            rot 1+ -rot
+        then
+    loop 2drop ;
+
+create color-count max-colors 2* allot
+
+: pegs ( cw -- c1,c2,… )
+    max-pegs 1- 0 do
+        10 /mod
+    loop ;
+
+: count-colors ( c1,c2,…,addr -- )
+    max-pegs 0 do
+        swap 1- over + c++!
+    loop drop ;
+
+: hits ( cw1,cw2 -- n )
+    color-count max-colors 2* erase
+    pegs color-count count-colors
+    pegs color-count max-colors + count-colors
+    0 max-colors 0 do
+        color-count i + c@
+        color-count max-colors + i + c@
+        min +
+    loop ;
+
+: misses ( cw1,cw2 -- n )
+    2dup hits -rot matches - ;
+
+: match  ( cw1, cw2 -- result )
+    2dup matches 10 *
+    -rot misses + ;
+
+55 constant max-results \ max of results with 9 colors
+
+create result-scores max-results cells allot
+
+: init-results
+    result-scores max-results cells erase ;
+
+codewords all-codewords
+
+: max-match-result ( cw,cws -- n )
+    init-results
+    dup codewords-start!
+    begin
+        2dup next-codeword ?dup while
+        match cells result-scores + ++!
+    repeat 
+    2drop drop
+    0 max-results 0 do
+        result-scores i cells + @ max
+    loop ;
+
+variable min-max-result
+variable min-max-codeword
+
+: min-max-match-result ( cws -- cw )
+    all-codewords all-codewords!
+    1000000 min-max-result !
+    begin
+        all-codewords next-codeword ?dup while      \ cws,cw
+        swap 2dup max-match-result                  \ cw,cws,r
+        -rot 2dup codeword-member? if               \ r,cw,cws
+            rot 2*
+        else
+            rot 2* 1+
+        then                                        \ cw,cws,r'
+        dup min-max-result @ < if
+            min-max-result !
+            swap min-max-codeword !                 \ cws
+        else                                        \ cw,cws,r'
+            rot 2drop                               \ cws
+        then
+    repeat drop
+    min-max-codeword @ ;
+
+variable nb-guesses
+10 constant max-guesses
+
+variable secret
+create guesses max-guesses cells allot
+
+: guess ( i -- cw )
+    cells guesses + @ ;
+
+create results max-guesses allot
+
+: result ( i -- r )
+    results + c@ ;
+
+: .results
+    nb-guesses @ 0 do
+        i 1+ 2 .r space
+        i guess .
+        i result . cr
+    loop ;
+
+codewords solutions
+
+: remove-diff-result ( cw,r,cws -- )
+    -rot 2>r
+    dup codewords-start!
+    begin
+        dup next-codeword ?dup while  \ cws,ncw
+        dup 2r@                       \ cws,ncw,ncw,cw,r
+        -rot match <> if              \ cws,ncw,f
+            over codeword-remove
+        else
+            drop
+        then
+    repeat drop 2r> 2drop ;
+
+: guess-codeword ( cw -- )
+    solutions all-codewords!
+    1122
+    max-guesses 0 do
+        dup secret @ match
+        2dup results i + c!
+        guesses i cells + !
+        solutions remove-diff-result
+        i result 40 = if
+            i 1+ nb-guesses !
+            leave
+        then
+        solutions min-max-match-result
+    loop ;
+
+: valid-codeword? ( n -- f )
+    dup codeword>number
+    number>codeword = ;
+    
+
+
--- test/mastermind-test.fs
+++ test/mastermind-test.fs
@@ -3,104 +3,22 @@ require ffl/tst.fs
 
 page
 
-\ some constants
+\ some values
 
-max-colors 6 ?s
-max-pegs 4 ?s
-max-codewords 1296 ?s \ 6⁴ 
+max-colors 6 ?s max-pegs 4 ?s first-codeword 1111 ?s
+6 (first-codeword) 111111 ?s
 
-\ number to codeword correspondance
-\ 0 ↔ 1111, 1 ↔ 1112, … 1295 ↔ 6666
+\ last-codeword
 
-0 number>codeword 1111 ?s
-1 number>codeword 1112 ?s
-6 number>codeword 1121 ?s
-35 number>codeword 1166 ?s
-1295 number>codeword 6666 ?s
+last-codeword 6666 ?s
 
-1111 codeword>number 0 ?s
-1112 codeword>number 1 ?s
-6666 codeword>number 1295 ?s
+\ next codeword
 
-\ number to offset and bits in a set
-0 offset,bits 1 ?s 0 ?s
-1 offset,bits 2 ?s 0 ?s
-2 offset,bits 4 ?s 0 ?s
-255 offset,bits 128 ?s 31 ?S
-389 offset,bits 32 ?s 48 ?s
+1111 next-codeword 1112 ?s
+1112 next-codeword 1113 ?s
+1116 next-codeword 1121 ?s
 
-\ bitset : inserting, removing, member
 
-create my-set 10 allot
-my-set 10 erase
 
-0 my-set bitset-member? ?false
-0 my-set bitset-insert
-0 my-set bitset-member? ?true
-4 my-set bitset-insert
-0 my-set bitset-remove
-4 my-set bitset-member? ?true
-0 my-set bitset-member? ?false
-
-\ codewords set : full, empty, inserting, removing, member, iterate
-
-codewords my-cws
-my-cws all-codewords!
-my-cws next-codeword 1111 ?s
-my-cws next-codeword 1112 ?s
-my-cws next-codeword 1113 ?s
-1111 my-cws codeword-member? ?true
-6666 my-cws codeword-member? ?true
-2222 my-cws codeword-member? ?true
-2222 my-cws codeword-remove
-2222 my-cws codeword-member? ?false
-my-cws empty-codewords!
-1111 my-cws codeword-member? ?false
-6666 my-cws codeword-member? ?false
-4444 my-cws codeword-insert
-4444 my-cws codeword-member? ?true
-1111 my-cws codeword-insert
-my-cws codewords-start!
-my-cws next-codeword 1111 ?s
-my-cws next-codeword 4444 ?s
-my-cws next-codeword 0 ?s
-
-\ matches, hits, misses, and match
-
-4264 1234 matches 2 ?s
-4264 4264 matches 4 ?s
-1111 4264 matches 0 ?s
-
-4264 6314 hits 2 ?s
-1264 6314 hits 3 ?s
-1264 1264 hits 4 ?s
-5555 1264 hits 0 ?s
-
-1264 6314 misses 2 ?s
-1264 5555 misses 0 ?s
-1264 1264 misses 0 ?s
-1122 2211 misses 4 ?s
-
-1264 6314 match 12 ?s
-1264 5555 match 00 ?s
-1264 1264 match 40 ?s
-1122 2211 match 04 ?s
-
-\ maximum of match results for a given codeword
-my-cws all-codewords!
-1122 my-cws max-match-result 256 ?s
-
-\ codeword with minimum of maximums of match results for a given codeword set
-my-cws all-codewords!
-my-cws min-max-match-result 1122 ?s
-
-\ guessing the secret codeword
-6415 secret !
-guess-codeword
-nb-guesses @ 5 ?s
-0 guess 1122 ?s
-0 result 1 ?s
-1 guess 2344 ?s
-1 result 1 ?s 
 bye
 
